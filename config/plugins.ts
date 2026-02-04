a351957fa6a01bcfbf52eee53f1a6f55010c5750
Kevin Busqueta <kevin.busqueta@gmail.com>
2026-02-04 15:27:03 +0100

    Fix R2 upload configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -12,12 +12,14 @@ export default ({ env }) => ({
         config: {
             provider: 'aws-s3',
             providerOptions: {
-                accessKeyId: env('R2_ACCESS_KEY_ID'),
-                secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
-                region: env('R2_REGION', 'auto'),
-                endpoint: env('R2_ENDPOINT'),
-                params: {
-                    Bucket: env('R2_BUCKET'),
+                s3Options: {
+                    accessKeyId: env('R2_ACCESS_KEY_ID'),
+                    secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+                    region: env('R2_REGION', 'auto'),
+                    endpoint: env('R2_ENDPOINT'),
+                    params: {
+                        Bucket: env('R2_BUCKET'),
+                    },
                 },
             },
             actionOptions: {
