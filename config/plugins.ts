9629791eb7e9f01ea1c8717936f6c18e35d78fd5
Kevin Busqueta <kevin.busqueta@gmail.com>
2026-02-04 15:35:22 +0100

    Add R2 public URL and ACL configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -12,6 +12,7 @@ export default ({ env }) => ({
         config: {
             provider: 'aws-s3',
             providerOptions: {
+                baseUrl: env('R2_PUBLIC_URL'),
                 s3Options: {
                     accessKeyId: env('R2_ACCESS_KEY_ID'),
                     secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
@@ -19,6 +20,7 @@ export default ({ env }) => ({
                     endpoint: env('R2_ENDPOINT'),
                     params: {
                         Bucket: env('R2_BUCKET'),
+                        ACL: 'public-read',
                     },
                 },
             },
